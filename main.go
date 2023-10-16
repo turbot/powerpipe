807b3ec463a639e5dbef7b1a08007a7f858ed7cb
Puskar Basu <puskar@turbot.com>
2023-10-16 14:42:48 +0800

    powerpipe initial main.go

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+
+	"github.com/turbot/go-kit/helpers"
+	"github.com/turbot/powerpipe/pkg/cmd"
+	"github.com/turbot/powerpipe/pkg/utils"
+	"github.com/turbot/steampipe/pkg/error_helpers"
+)
+
+var exitCode int
 
 func main() {
-	fmt.Println("Hello, Powerpipe!")
+	ctx := context.Background()
+	utils.LogTime("main start")
+	defer func() {
+		if r := recover(); r != nil {
+			error_helpers.ShowError(ctx, helpers.ToError(r))
+			if exitCode == 0 {
+				exitCode = 255
+			}
+		}
+		utils.LogTime("main end")
+		utils.DisplayProfileData()
+		os.Exit(exitCode)
+	}()
+	cmd.InitCmd()
+
+	// execute the command
+	exitCode = cmd.Execute()
 }
