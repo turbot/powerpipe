ac91f0792ed1137a74a6ece17e90c24dd3e77726
Binaek Sarkar <binaek@turbot.com>
2024-01-05 20:46:40 +0530

    build constants

---
+++ internal/constants/build.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	ConfigKeyVersion = "main.version"
+	ConfigKeyCommit  = "main.commit"
+	ConfigKeyDate    = "main.date"
+	ConfigKeyBuiltBy = "main.builtBy"
+
+	LocalBuild = "local"
+)
