43904f0841f215edb64676e7f01f9676e0ad709a
Binaek Sarkar <binaek@turbot.com>
2024-01-15 20:58:57 +0530

    Adds MySQL support. Closes #66

--- internal/db_client/init.go
+++ internal/db_client/init.go
@@ -2,7 +2,7 @@ package db_client
 
 import (
 	// database connection drivers
-	//_ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	_ "github.com/mattn/go-sqlite3"
 )
