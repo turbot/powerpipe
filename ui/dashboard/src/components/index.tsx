bd04ef8a681a62d01173ee21f61846f5f6836734
Mike Burgess <mike@turbot.com>
2024-01-23 12:57:31 +0000

    Allow override of dashboard snapshot header component

--- ui/dashboard/src/components/index.tsx
+++ ui/dashboard/src/components/index.tsx
@@ -1,9 +1,13 @@
 import DashboardListEmptyCallToAction from "./DashboardListEmptyCallToAction";
 import { ComponentsMap } from "../types";
+import { getComponent } from "components/dashboards";
 
 const buildComponentsMap = (overrides = {}): ComponentsMap => {
+  const SnapshotHeader = getComponent("snapshot_header");
+  
   return {
     DashboardListEmptyCallToAction,
+    SnapshotHeader,
     ...overrides,
   };
 };
