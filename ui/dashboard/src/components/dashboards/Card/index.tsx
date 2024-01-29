840222360c82bc2a430ddb5aa8d2dbeccfcb28d7
Mike Burgess <mike@turbot.com>
2024-01-29 22:15:26 +0000

    Card overhaul WIP - need to fix loading state and truncate

--- ui/dashboard/src/components/dashboards/Card/index.tsx
+++ ui/dashboard/src/components/dashboards/Card/index.tsx
@@ -12,17 +12,16 @@ import {
   CardDiffState,
   CardType,
 } from "../data/CardDataProcessor";
-import { classNames } from "../../../utils/styles";
-import { PanelDefinition, PanelProperties } from "../../../types";
+import { classNames } from "utils/styles";
+import { PanelDefinition, PanelProperties } from "types";
 import { getComponent, registerComponent } from "../index";
 import {
   getIconClasses,
-  getTextClasses,
+  getIconStyles,
+  getIconWrapperClasses,
   getWrapperClasses,
-} from "../../../utils/card";
+} from "utils/card";
 import { IDiffProperties } from "../data/types";
-import { ThemeNames } from "../../../hooks/useTheme";
-import { useDashboard } from "../../../hooks/useDashboard";
 import { useEffect, useState } from "react";
 
 const Table = getComponent("table");
@@ -40,7 +39,7 @@ export type CardProps = PanelProperties &
     display_type?: CardType;
     properties: CardProperties;
   } & {
-    diff_panel: PanelDefinition;
+    diff_panel?: PanelDefinition;
   };
 
 type CardState = {
@@ -107,6 +106,18 @@ const Label = ({ value }) => {
   return value;
 };
 
+const Value = ({ value }) => {
+  if (!value) {
+    return null;
+  }
+
+  if (isNumber(value)) {
+    return <IntegerDisplay num={value} startAt="100k" />;
+  }
+
+  return <Label value={value} />;
+};
+
 const CardDiffDisplay = ({ diff }: CardDiffDisplayProps) => {
   if (!diff || diff.direction === "none") {
     return null;
@@ -144,10 +155,6 @@ const Card = (props: CardProps) => {
   const [renderedHref, setRenderedHref] = useState<string | null>(
     state.href || null,
   );
-  const textClasses = getTextClasses(state.type);
-  const {
-    themeContext: { theme },
-  } = useDashboard();
   const { ready: templateRenderReady, renderTemplates } = useTemplateRender();
 
   useEffect(() => {
@@ -198,77 +205,178 @@ const Card = (props: CardProps) => {
   const card = (
     <div
       className={classNames(
-        "relative pt-4 px-3 pb-4 sm:px-4 rounded-md overflow-hidden",
+        "overflow-hidden bg-dashboard-panel text-foreground print:bg-white print:text-black shadow-sm p-3 pr-5",
         getWrapperClasses(state.type),
+        !state.icon ? "pl-4" : undefined,
       )}
     >
-      <dt>
-        <div className="absolute">
-          <DashboardIcon
-            className={classNames(getIconClasses(state.type), "h-8 w-8")}
-            icon={state.icon}
-          />
-        </div>
-        <p
-          className={classNames(
-            "text-sm font-semibold truncate text-foreground",
-            state.icon ? "ml-11" : "ml-2",
-          )}
-          title={state.label || undefined}
-        >
-          {state.loading && "Loading..."}
-          {!state.loading && !state.label && (
-            <DashboardIcon
-              className="h-5 w-5"
-              icon="materialsymbols-outline:remove"
-            />
-          )}
-          {!state.loading && state.label}
-        </p>
-      </dt>
-      <dd
-        className={classNames(
-          "flex items-baseline space-x-4",
-          state.icon ? "ml-11" : "ml-2",
-        )}
-        title={state.value || undefined}
-      >
-        <p
-          className={classNames(
-            "text-4xl mt-1 font-semibold text-left truncate",
-            textClasses,
-          )}
-        >
-          {state.loading && (
-            <LoadingIndicator
-              className={classNames(
-                "h-9 w-9 mt-1",
-                theme.name === ThemeNames.STEAMPIPE_DEFAULT
-                  ? "text-black-scale-4"
-                  : null,
-              )}
-            />
-          )}
-          {!state.loading &&
-            (state.value === null || state.value === undefined) && (
+      <div className="flex space-x-3">
+        {(state.loading || state.icon) && (
+          <div
+            className={classNames(
+              "shrink-0 grow-0",
+              //getIconWrapperClasses(state.type),
+            )}
+          >
+            {state.loading ? (
+              <LoadingIndicator className="h-8 w-8" />
+            ) : (
               <DashboardIcon
-                className="h-10 w-10"
-                icon="materialsymbols-outline:remove"
+                className={classNames("h-8 w-8", getIconClasses(state.type))}
+                aria-hidden="true"
+                icon={state.icon}
+                style={getIconStyles(state.type)}
               />
             )}
-          {state.value !== null &&
-            state.value !== undefined &&
-            !isNumber(state.value) && <Label value={state.value} />}
-          {isNumber(state.value) && (
-            <>
-              <IntegerDisplay num={state.value} startAt="100k" />
-            </>
-          )}
-        </p>
-        <CardDiffDisplay diff={state.diff} />
-      </dd>
+          </div>
+        )}
+        <div className="grow mt-0.5">
+          <dt className="text-lg">
+            <p className="truncate">
+              {state.loading ? "Loading..." : state.label}
+            </p>
+          </dt>
+          <dd className="font-semibold text-3xl mt-1 mb-1">
+            <Value value={state.value} />
+          </dd>
+        </div>
+      </div>
+      {/*<dt>*/}
+      {/*  {state.icon && (*/}
+      {/*    <div*/}
+      {/*      className={classNames(*/}
+      {/*        "absolute rounded-md p-3",*/}
+      {/*        getWrapperClasses(state.type),*/}
+      {/*      )}*/}
+      {/*    >*/}
+      {/*      <DashboardIcon*/}
+      {/*        className="h-6 w-6 text-white"*/}
+      {/*        aria-hidden="true"*/}
+      {/*        icon={state.icon}*/}
+      {/*        style={getIconStyles(state.type)}*/}
+      {/*      />*/}
+      {/*    </div>*/}
+      {/*  )}*/}
+      {/*  <p className="ml-16 truncate text-sm font-medium text-gray-500">*/}
+      {/*    {state.label}*/}
+      {/*  </p>*/}
+      {/*</dt>*/}
+      {/*<dd className="ml-16 flex items-baseline pb-6 sm:pb-7">*/}
+      {/*  <p className="text-2xl font-semibold text-gray-900">{state.value}</p>*/}
+      {/*  /!*<p*!/*/}
+      {/*  /!*  className={classNames(*!/*/}
+      {/*  /!*    item.changeType === "increase" ? "text-green-600" : "text-red-600",*!/*/}
+      {/*  /!*    "ml-2 flex items-baseline text-sm font-semibold",*!/*/}
+      {/*  /!*  )}*!/*/}
+      {/*  /!*>*!/*/}
+      {/*  /!*  {item.changeType === "increase" ? (*!/*/}
+      {/*  /!*    <ArrowUpIcon*!/*/}
+      {/*  /!*      className="h-5 w-5 flex-shrink-0 self-center text-green-500"*!/*/}
+      {/*  /!*      aria-hidden="true"*!/*/}
+      {/*  /!*    />*!/*/}
+      {/*  /!*  ) : (*!/*/}
+      {/*  /!*    <ArrowDownIcon*!/*/}
+      {/*  /!*      className="h-5 w-5 flex-shrink-0 self-center text-red-500"*!/*/}
+      {/*  /!*      aria-hidden="true"*!/*/}
+      {/*  /!*    />*!/*/}
+      {/*  /!*  )}*!/*/}
+
+      {/*  /!*  <span className="sr-only">*!/*/}
+      {/*  /!*    {" "}*!/*/}
+      {/*  /!*    {item.changeType === "increase" ? "Increased" : "Decreased"} by{" "}*!/*/}
+      {/*  /!*  </span>*!/*/}
+      {/*  /!*  {item.change}*!/*/}
+      {/*  /!*</p>*!/*/}
+      {/*  /!*<div className="absolute inset-x-0 bottom-0 bg-gray-50 px-4 py-4 sm:px-6">*!/*/}
+      {/*  /!*  <div className="text-sm">*!/*/}
+      {/*  /!*    <a*!/*/}
+      {/*  /!*      href="#"*!/*/}
+      {/*  /!*      className="font-medium text-indigo-600 hover:text-indigo-500"*!/*/}
+      {/*  /!*    >*!/*/}
+      {/*  /!*      View all<span className="sr-only"> {item.name} stats</span>*!/*/}
+      {/*  /!*    </a>*!/*/}
+      {/*  /!*  </div>*!/*/}
+      {/*  /!*</div>*!/*/}
+      {/*</dd>*/}
     </div>
   );
+
+  // const card = (
+  //   <div
+  //     className={classNames(
+  //       "relative pt-4 px-3 pb-4 sm:px-4 rounded-md overflow-hidden",
+  //       getWrapperClasses(state.type),
+  //     )}
+  //   >
+  //     <dt>
+  //       <div className="absolute">
+  //         <DashboardIcon
+  //           className={classNames(getIconClasses(state.type), "h-8 w-8")}
+  //           icon={state.icon}
+  //         />
+  //       </div>
+  //       <p
+  //         className={classNames(
+  //           "text-sm font-semibold truncate text-foreground",
+  //           state.icon ? "ml-11" : "ml-2",
+  //         )}
+  //         title={state.label || undefined}
+  //       >
+  //         {state.loading && "Loading..."}
+  //         {!state.loading && !state.label && (
+  //           <DashboardIcon
+  //             className="h-5 w-5"
+  //             icon="materialsymbols-outline:remove"
+  //           />
+  //         )}
+  //         <span className={getTextHeaderClasses(state.type)}>
+  //           {!state.loading && state.label}
+  //         </span>
+  //       </p>
+  //     </dt>
+  //     <dd
+  //       className={classNames(
+  //         "flex items-baseline space-x-4",
+  //         state.icon ? "ml-11" : "ml-2",
+  //       )}
+  //       title={state.value || undefined}
+  //     >
+  //       <p
+  //         className={classNames(
+  //           "text-4xl mt-1 font-semibold text-left truncate",
+  //           textClasses,
+  //         )}
+  //       >
+  //         {state.loading && (
+  //           <LoadingIndicator
+  //             className={classNames(
+  //               "h-9 w-9 mt-1",
+  //               theme.name === ThemeNames.STEAMPIPE_DEFAULT
+  //                 ? "text-black-scale-4"
+  //                 : null,
+  //             )}
+  //           />
+  //         )}
+  //         {!state.loading &&
+  //           (state.value === null || state.value === undefined) && (
+  //             <DashboardIcon
+  //               className="h-10 w-10"
+  //               icon="materialsymbols-outline:remove"
+  //             />
+  //           )}
+  //         {state.value !== null &&
+  //           state.value !== undefined &&
+  //           !isNumber(state.value) && <Label value={state.value} />}
+  //         {isNumber(state.value) && (
+  //           <>
+  //             <IntegerDisplay num={state.value} startAt="100k" />
+  //           </>
+  //         )}
+  //       </p>
+  //       <CardDiffDisplay diff={state.diff} />
+  //     </dd>
+  //   </div>
+  // );
 
   if (renderedHref) {
     return <ExternalLink to={renderedHref}>{card}</ExternalLink>;
--- ui/dashboard/src/utils/card.ts
+++ ui/dashboard/src/utils/card.ts
@@ -1,21 +1,5 @@
 import { classNames } from "./styles";
 
-const getIconClasses = (type) => {
-  const coloredClasses = "text-3xl";
-  switch (type) {
-    case "alert":
-      return classNames(coloredClasses, "text-alert");
-    case "info":
-      return classNames(coloredClasses, "text-info");
-    case "ok":
-      return classNames(coloredClasses, "text-ok");
-    case "severity":
-      return classNames(coloredClasses, "text-severity");
-    default:
-      return "text-black-scale-4 text-3xl";
-  }
-};
-
 const getIconForType = (type, icon) => {
   if (!type && !icon) {
     return null;
@@ -28,45 +12,103 @@ const getIconForType = (type, icon) => {
   switch (type) {
     case "alert":
       return "materialsymbols-solid:error";
+    // return "materialsymbols-solid:priority_high";
     case "ok":
-      return "materialsymbols-solid:check_circle";
+      // return "materialsymbols-solid:check_circle";
+      return "materialsymbols-solid:check";
     case "info":
-      return "materialsymbols-solid:info";
+      // return "materialsymbols-solid:info";
+      return "materialsymbols-solid:info_i";
     case "severity":
       return "materialsymbols-solid:warning";
+    case "skip":
+      return "materialsymbols-solid:arrow_right_alt";
     default:
       return null;
   }
 };
 
-const getTextClasses = (type) => {
+const getIconStyles = (type) => {
+  if (!type) {
+    return {};
+  }
+
   switch (type) {
     case "alert":
-      return "text-alert";
-    case "info":
-      return "text-info";
+      return { fontWeight: "bold" };
     case "ok":
-      return "text-ok";
+      return { fontVariationSettings: "'wght' 700" };
+    case "info":
+      return {};
     case "severity":
-      return "text-severity";
+      return {};
+    case "skip":
+      return {};
     default:
-      return null;
+      return {};
+  }
+};
+
+const getIconClasses = (type) => {
+  const baseClasses = "text-3xl opacity-100";
+  switch (type) {
+    case "alert":
+      return classNames(baseClasses, "text-alert");
+    case "info":
+      return classNames(baseClasses, "text-info");
+    case "ok":
+      return classNames(baseClasses, "text-ok");
+    case "severity":
+      return classNames(baseClasses, "text-severity");
+    default:
+      return classNames(baseClasses, "text-skip");
+  }
+};
+
+const getIconWrapperClasses = (type) => {
+  const baseClasses = "flex items-center justify-center h-9 w-9 rounded-full";
+  switch (type) {
+    case "alert":
+      return classNames(baseClasses, "bg-alert/15");
+    case "info":
+      return classNames(baseClasses, "bg-info/15");
+    case "ok":
+      return classNames(baseClasses, "bg-ok/15");
+    case "severity":
+      return classNames(baseClasses, "bg-severity/15");
+    default:
+      return classNames(baseClasses, "bg-skip/15");
   }
 };
 
 const getWrapperClasses = (type) => {
+  const baseClasses = "border-l-4 rounded-r-md"; // rounded-l-md rounded-r-md rounded-b-md";
+  // const baseClasses = "rounded-md"; // rounded-l-md rounded-r-md rounded-b-md";
   switch (type) {
     case "alert":
-      return "bg-dashboard-panel border border-alert";
+      // return classNames(baseClasses, "bg-alert/5");
+      return classNames(baseClasses, "border-alert");
     case "info":
-      return "bg-dashboard-panel border border-info";
+      // return classNames(baseClasses, "bg-info/5");
+      return classNames(baseClasses, "border-info");
     case "ok":
-      return "bg-dashboard-panel border border-ok ";
+      // return classNames(baseClasses, "bg-ok/5");
+      return classNames(baseClasses, "border-ok");
     case "severity":
-      return "bg-dashboard-panel border border-severity";
+      // return classNames(baseClasses, "bg-severity/5");
+      return classNames(baseClasses, "border-severity");
+    case "skip":
+      // return classNames(baseClasses, "bg-skip/5");
+      return classNames(baseClasses, "border-skip");
     default:
-      return "bg-dashboard-panel shadow-sm border border-gray-400";
+      return "rounded-md";
   }
 };
 
-export { getIconClasses, getIconForType, getTextClasses, getWrapperClasses };
+export {
+  getIconClasses,
+  getIconForType,
+  getIconStyles,
+  getIconWrapperClasses,
+  getWrapperClasses,
+};
