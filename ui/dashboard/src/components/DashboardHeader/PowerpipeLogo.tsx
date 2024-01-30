a13a48a0301ffc4e59a94ef9b10b51ea79d23bee
Mike Burgess <mike@turbot.com>
2024-01-30 17:07:14 +0000

    Update Powerpipe logo in dashboard UI

--- ui/dashboard/src/components/DashboardHeader/PowerpipeLogo.tsx
+++ ui/dashboard/src/components/DashboardHeader/PowerpipeLogo.tsx
@@ -2,11 +2,13 @@ import { getComponent } from "../dashboards";
 // @ts-ignore
 import { ReactComponent as Logo } from "./logos/powerpipe-logo.svg";
 // @ts-ignore
-import { ReactComponent as LogoWordmarkColor } from "./logos/powerpipe-logo-wordmark-color.svg";
+import { ReactComponent as LogoDarkmode } from "./logos/powerpipe-logo-darkmode.svg";
 // @ts-ignore
-import { ReactComponent as LogoWordmarkDark } from "./logos/powerpipe-logo-wordmark-darkmode.svg";
-import { ThemeNames } from "../../hooks/useTheme";
-import { useDashboard } from "../../hooks/useDashboard";
+import { ReactComponent as LogoWordmark } from "./logos/powerpipe-logo-wordmark.svg";
+// @ts-ignore
+import { ReactComponent as LogoWordmarkDarkmode } from "./logos/powerpipe-logo-wordmark-darkmode.svg";
+import { ThemeNames } from "hooks/useTheme";
+import { useDashboard } from "hooks/useDashboard";
 
 const PowerpipeLogo = () => {
   const {
@@ -18,11 +20,12 @@ const PowerpipeLogo = () => {
     <div className="mr-1 md:mr-4">
       <ExternalLink ignoreDataMode to="/">
         <div className="block md:hidden w-8">
-          <Logo />
+          {theme.name === ThemeNames.STEAMPIPE_DEFAULT && <Logo />}
+          {theme.name === ThemeNames.STEAMPIPE_DARK && <LogoDarkmode />}
         </div>
         <div className="hidden md:block w-48">
-          {theme.name === ThemeNames.STEAMPIPE_DEFAULT && <LogoWordmarkColor />}
-          {theme.name === ThemeNames.STEAMPIPE_DARK && <LogoWordmarkDark />}
+          {theme.name === ThemeNames.STEAMPIPE_DEFAULT && <LogoWordmark />}
+          {theme.name === ThemeNames.STEAMPIPE_DARK && <LogoWordmarkDarkmode />}
         </div>
       </ExternalLink>
     </div>
