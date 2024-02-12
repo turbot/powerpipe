aa51ceee9e3f12d4d85448262f9e9bfd1ccf7e9e
Puskar Basu <puskar@turbot.com>
2024-02-12 16:51:54 +0530

    Update mysql mod query

--- tests/acceptance/test_data/mods/mysql_mod/query.pp
+++ tests/acceptance/test_data/mods/mysql_mod/query.pp
@@ -1,6 +1,6 @@
 query "total_employee" {
   sql = <<-EOQ
-    SELECT 1;
+    SELECT ? as "Params", ? as "Params2";
   EOQ
   param "p1"{
     description = "First parameter"
@@ -10,10 +10,21 @@ query "total_employee" {
     description = "Second parameter"
     default = "default_parameter_2"
   }
-  param "p3"{
-    description = "Third parameter"
-    default = "default_parameter_3"
-  }
 }
-    // SELECT COUNT(*) AS "Total Employees", CONCAT(? , ' ', ? , ' ', ?) as "Params" FROM employee;
-    // SELECT COUNT(*) AS "Total Employees", CONCAT(@p1, ' ', @p2, ' ', @p3) as "Params" FROM employee;
+// query "total_employee" {
+//   sql = <<-EOQ
+//     SELECT COUNT(*) AS "Total Employees", CONCAT(? , ' ', ? , ' ', ?) as "Params" FROM employee;
+//   EOQ
+//   param "p1"{
+//     description = "First parameter"
+//     default = "default_parameter_1"
+//   }
+//   param "p2"{
+//     description = "Second parameter"
+//     default = "default_parameter_2"
+//   }
+//   param "p3"{
+//     description = "Third parameter"
+//     default = "default_parameter_3"
+//   }
+// }
