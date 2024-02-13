31984df7f8a1b0c4bd32972a50a468465a154587
Puskar Basu <puskar@turbot.com>
2024-02-13 12:03:56 +0530

    Implement env vars

--- internal/constants/env.go
+++ internal/constants/env.go
@@ -1,7 +1,26 @@
 package constants
 
 const (
+	EnvPipesHost        = "PIPES_HOST"
+	EnvPipesToken       = "PIPES_TOKEN"
+	EnvCloudHost        = "POWERPIPE_CLOUD_HOST"
+	EnvCloudToken       = "POWERPIPE_CLOUD_TOKEN"
+	EnvConfigPath       = "POWERPIPE_CONFIG_PATH"
+	EnvHost             = "POWERPIPE_HOST"
+	EnvInstallDir       = "POWERPIPE_INSTALL_DIR"
+	EnvListen           = "POWERPIPE_LISTEN"
+	EnvLogLevel         = "POWERPIPE_LOG_LEVEL"
+	EnvMaxParallel      = "POWERPIPE_MAX_PARALLEL"
+	EnvMemoryMaxMb      = "POWERPIPE_MEMORY_MAX_MB"
+	EnvModLocation      = "POWERPIPE_MOD_LOCATION"
+	EnvPort             = "POWERPIPE_PORT"
+	EnvQueryTimeout     = "POWERPIPE_QUERY_TIMEOUT"
+	EnvSnapshotLocation = "POWERPIPE_SNAPSHOT_LOCATION"
+	EnvTelemetry        = "POWERPIPE_TELEMETRY"
+	EnvUpdateCheck      = "POWERPIPE_UPDATE_CHECK"
+	EnvDatabase         = "POWERPIPE_DATABASE"
+	EnvWorkspaceProfile = "POWERPIPE_WORKSPACE"
 	// TODO KAI KEEP THIS?
 	// EnvConfigDump is an undocumented variable is subject to change in the future
-	EnvConfigDump = "STEAMPIPE_CONFIG_DUMP"
+	EnvConfigDump = "POWERPIPE_CONFIG_DUMP"
 )
