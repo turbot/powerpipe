d072a57a9cc6bd499f659b36d94a10954714cf18
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2024-03-05 21:32:37 +0530

    Update CLI descriptions (#233)

--- internal/constants/descriptions.go
+++ internal/constants/descriptions.go
@@ -1,7 +1,24 @@
 package constants
 
-// TODO
 const (
-	PowerpipeShortDescription = "Powerpipe"
-	PowerpipeLongDescription  = "Powerpipe."
+	PowerpipeShortDescription = "Powerpipe: Dashboards for DevOps."
+	PowerpipeLongDescription  = `Powerpipe: Dashboards for DevOps.
+	
+Visualize cloud configurations. Assess security posture against a
+massive library of benchmarks. Build custom dashboards with code.
+	
+Common commands:
+	
+  # Install a mod from the hub - https://hub.powerpipe.io
+  powerpipe mod init
+  powerpipe mod install github.com/turbot/powerpipe-mod-aws-compliance
+
+  # View dashboards at http://localhost:9033
+  powerpipe server
+
+  # List and run benchmarks in the terminal
+  powerpipe benchmark list
+  powerpipe benchmark run aws_compliance.benchmark.cis_v140
+	
+Documentation available at https://powerpipe.io/docs`
 )
