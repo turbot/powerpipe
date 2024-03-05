725873e0d461168f2c3559ab718dbde3d998b10a
Puskar Basu <puskar@turbot.com>
2024-03-05 16:38:17 +0530

    Update CLI descriptions

--- internal/constants/descriptions.go
+++ internal/constants/descriptions.go
@@ -1,7 +1,11 @@
 package constants
 
-// TODO
 const (
-	PowerpipeShortDescription = "Powerpipe"
-	PowerpipeLongDescription  = "Powerpipe."
+	PowerpipeShortDescription = "Powerpipe: Dashboards for DevOps."
+	PowerpipeLongDescription  = `Powerpipe: Dashboards for DevOps.
+	
+Visualize cloud configurations. Assess security posture against a
+massive library of benchmarks. Build custom dashboards with code.
+
+Documentation available at https://powerpipe.io/docs`
 )
