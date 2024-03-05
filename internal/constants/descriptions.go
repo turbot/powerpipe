f662eaadce1708a9ef21ea0cad4a317fae3f72a8
Puskar Basu <puskar@turbot.com>
2024-03-05 21:04:38 +0530

    updated desc

--- internal/constants/descriptions.go
+++ internal/constants/descriptions.go
@@ -6,6 +6,19 @@ const (
 	
 Visualize cloud configurations. Assess security posture against a
 massive library of benchmarks. Build custom dashboards with code.
+	
+Common commands:
+	
+  # Install a mod from the hub - https://hub.powerpipe.io
+  powerpipe mod init
+  powerpipe mod install github.com/turbot/powerpipe-mod-aws-compliance
 
+  # View dashboards at http://localhost:9033
+  powerpipe server
+
+  # List and run benchmarks in the terminal
+  powerpipe benchmark list
+  powerpipe benchmark run aws_compliance.benchmark.cis_v140
+	
 Documentation available at https://powerpipe.io/docs`
 )
