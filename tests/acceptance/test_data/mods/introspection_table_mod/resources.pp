5bfd1c78b717560317ae7abc2e153dbc1538dbe6
Puskar Basu <puskar@turbot.com>
2024-03-20 17:09:24 +0530

    remove nested dashboard

--- tests/acceptance/test_data/mods/introspection_table_mod/resources.pp
+++ tests/acceptance/test_data/mods/introspection_table_mod/resources.pp
@@ -131,9 +131,3 @@ dashboard "sample_dashboard_1" {
 card "sample_card_2" {
 	title = "Sample card 2"
 }
-
-dashboard "compose_other" {
-  dashboard "reused_sample_dashboard_1" {
-    base = introspection_table_mod.dashboard.sample_dashboard_1
-  }
-}
