78fb92e7bc623d91f63768a37cc8c5cd4446c828
Puskar Basu <puskar@turbot.com>
2024-03-20 14:02:11 +0530

    add mod

---
+++ tests/acceptance/test_data/mods/introspection_table_mod/mod.pp
@@ -0,0 +1,4 @@
+mod "introspection_table_mod"{
+  title = "Introspection table test mod"
+  description = "This is a simple mod used for testing the introspection features from steampipe. Do not expand this mod. This mod also tests the list/show outputs of all resource types."
+}
