82c8dbe2d0ed622cb8294c20b2fbe55e14b23473
kai <kai@turbot.com>
2024-09-27 10:12:11 +0100

    timing streaming

--- internal/queryresult/result.go
+++ internal/queryresult/result.go
@@ -9,10 +9,14 @@ type TimingMetadata struct {
 	Duration time.Duration
 }
 
+// GetTiming implements TimingContainer - we implement this interface
+// to allow TimingMetadata to be used to parameterize the ResultStreamer
+func (t TimingMetadata) GetTiming() any {
+	return t
+}
+
 type Result = queryresult.Result[*TimingMetadata]
 
 func NewResult(cols []*queryresult.ColumnDef) *Result {
 	return queryresult.NewResult[*TimingMetadata](cols, &TimingMetadata{})
 }
-
-type SyncQueryResult = queryresult.SyncQueryResult[*TimingMetadata]
