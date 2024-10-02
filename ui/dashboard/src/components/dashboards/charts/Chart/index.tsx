2e60a9852f82793b53570c116c408ad8bbd52b81
Mike Burgess <mike@turbot.com>
2024-10-02 09:22:20 +0100

    Move search path prefix destructuring in Chart component to wrapper #325

--- ui/dashboard/src/components/dashboards/charts/Chart/index.tsx
+++ ui/dashboard/src/components/dashboards/charts/Chart/index.tsx
@@ -750,6 +750,7 @@ const buildChartOptions = (props: ChartProps, themeColors: any) => {
 
 type ChartComponentProps = {
   options: EChartsOption;
+  searchPathPrefix: string[];
   type: ChartType | FlowType | GraphType | HierarchyType;
 };
 
@@ -783,9 +784,8 @@ const handleClick = async (
   }
 };
 
-const Chart = ({ options, type }: ChartComponentProps) => {
+const Chart = ({ options, searchPathPrefix, type }: ChartComponentProps) => {
   const [echarts, setEcharts] = useState<any | null>(null);
-  const { searchPathPrefix } = useDashboard();
   const navigate = useNavigate();
   const chartRef = useRef<ReactEChartsCore>(null);
   const [imageUrl, setImageUrl] = useState<string | null>(null);
@@ -852,6 +852,7 @@ const Chart = ({ options, type }: ChartComponentProps) => {
 
 const ChartWrapper = (props: ChartProps) => {
   const {
+    searchPathPrefix,
     themeContext: { wrapperRef },
   } = useDashboard();
   const themeColors = useChartThemeColors();
@@ -867,6 +868,7 @@ const ChartWrapper = (props: ChartProps) => {
   return (
     <Chart
       options={buildChartOptions(props, themeColors)}
+      searchPathPrefix={searchPathPrefix}
       type={props.display_type || "column"}
     />
   );
