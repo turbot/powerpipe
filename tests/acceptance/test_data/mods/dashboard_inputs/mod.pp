9cfad175305811ac6e941f4bb5cda83dc2571771
Puskar Basu <puskar@turbot.com>
2024-11-11 15:57:54 +0700

    fix dashboard test

--- tests/acceptance/test_data/mods/dashboard_inputs/mod.pp
+++ tests/acceptance/test_data/mods/dashboard_inputs/mod.pp
@@ -1,5 +1,4 @@
 mod "dashboard_inputs"{
   title = "Dashboard using inputs"
   description = "Dashboard for testing inputs - running dashboard with --arg flag"
-  database = connection.tailpipe.tp
 }
