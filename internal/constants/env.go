46981a4e12501299ede5fadbeeeab337d8eebb20
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2024-11-21 18:11:04 +0530

    Implement POWERPIPE_DISPLAY_WIDTH env var to set the terminal width. Closes #154 (#574)
    * Implement POWERPIPE_DISPLAY_WIDTH env var to set the terminal width. Closes #154
    
    * update tests

--- internal/constants/env.go
+++ internal/constants/env.go
@@ -5,6 +5,7 @@ const (
 	EnvPort             = "POWERPIPE_PORT"
 	EnvBenchmarkTimeout = "POWERPIPE_BENCHMARK_TIMEOUT"
 	EnvDashboardTimeout = "POWERPIPE_DASHBOARD_TIMEOUT"
+	EnvDisplayWidth     = "POWERPIPE_DISPLAY_WIDTH"
 	// EnvConfigDump is an undocumented variable is subject to change in the future
 	EnvConfigDump = "POWERPIPE_CONFIG_DUMP"
 )
