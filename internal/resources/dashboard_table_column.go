a0312f6af2e6ad7b8c8c319100aa090adcb6638f
Puskar Basu <puskar@turbot.com>
2024-11-28 19:33:03 +0530

    Add primary_key property to DashboardTableColumn resource to specify primary keys in a table

--- internal/resources/dashboard_table_column.go
+++ internal/resources/dashboard_table_column.go
@@ -3,10 +3,11 @@ package resources
 import "github.com/turbot/pipe-fittings/utils"
 
 type DashboardTableColumn struct {
-	Name    string  `hcl:"name,label" json:"name" snapshot:"name"`
-	Display *string `cty:"display" hcl:"display" json:"display,omitempty" snapshot:"display"`
-	Wrap    *string `cty:"wrap" hcl:"wrap" json:"wrap,omitempty" snapshot:"wrap"`
-	HREF    *string `cty:"href" hcl:"href" json:"href,omitempty" snapshot:"href"`
+	Name       string  `hcl:"name,label" json:"name" snapshot:"name"`
+	Display    *string `cty:"display" hcl:"display" json:"display,omitempty" snapshot:"display"`
+	Wrap       *string `cty:"wrap" hcl:"wrap" json:"wrap,omitempty" snapshot:"wrap"`
+	HREF       *string `cty:"href" hcl:"href" json:"href,omitempty" snapshot:"href"`
+	PrimaryKey *bool   `cty:"primary_key" hcl:"primary_key" json:"primary_key,omitempty" snapshot:"primary_key"`
 }
 
 func (c DashboardTableColumn) Equals(other *DashboardTableColumn) bool {
@@ -17,5 +18,6 @@ func (c DashboardTableColumn) Equals(other *DashboardTableColumn) bool {
 	return utils.SafeStringsEqual(c.Name, other.Name) &&
 		utils.SafeStringsEqual(c.Display, other.Display) &&
 		utils.SafeStringsEqual(c.Wrap, other.Wrap) &&
-		utils.SafeStringsEqual(c.HREF, other.HREF)
+		utils.SafeStringsEqual(c.HREF, other.HREF) &&
+		utils.SafeBoolEqual(c.PrimaryKey, other.PrimaryKey)
 }
