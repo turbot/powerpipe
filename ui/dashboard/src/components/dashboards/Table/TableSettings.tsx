009338667f84779e31ebe9215e7a3cd6498a0fa8
Mike Burgess <mike@turbot.com>
2025-01-03 15:08:39 +0000

    Add ability to select all columns in column selector. Closes #617.

--- ui/dashboard/src/components/dashboards/Table/TableSettings.tsx
+++ ui/dashboard/src/components/dashboards/Table/TableSettings.tsx
@@ -1,12 +1,29 @@
+import Icon from "@powerpipe/components/Icon";
 import SearchInput from "@powerpipe/components/SearchInput";
 import sortBy from "lodash/sortBy";
 import useTableConfig from "@powerpipe/hooks/useTableConfig";
-import { classNames } from "@powerpipe/utils/styles";
+import { AsyncNoop } from "@powerpipe/types/func";
 import { Column, RowData } from "@tanstack/react-table";
 import { KeyValuePairs } from "@powerpipe/components/dashboards/common/types";
 import { useEffect, useMemo, useState } from "react";
 
-type TableSettingsColumnsViewType = "all" | "visible" | "hidden";
+const ShortcutSelector = ({
+  icon,
+  label,
+  action,
+}: {
+  icon: string;
+  label: string;
+  action: AsyncNoop;
+}) => (
+  <div
+    className="flex items-center space-x-1 cursor-pointer hover:text-foreground-light"
+    onClick={action}
+  >
+    <Icon icon={icon} className="h-5 w-5" />
+    <span>{label}</span>
+  </div>
+);
 
 const TableSettingsColumns = ({
   name,
@@ -17,8 +34,7 @@ const TableSettingsColumns = ({
 }) => {
   const { update } = useTableConfig(name);
   const [search, setSearch] = useState("");
-  const [view, setView] = useState<TableSettingsColumnsViewType>("all");
-  const { columnMap } = useMemo(() => {
+  const columnMap = useMemo(() => {
     const columnMap: KeyValuePairs<{
       column: Column<KeyValuePairs>;
       index: number;
@@ -26,7 +42,7 @@ const TableSettingsColumns = ({
       acc[column.id] = { column, index: currentIndex };
       return acc;
     }, {});
-    return { leafColumns, columnMap };
+    return columnMap;
   }, []);
   const [columnVisibility, setColumnVisibility] = useState<
     {
@@ -39,17 +55,9 @@ const TableSettingsColumns = ({
       visible: column.getIsVisible(),
     })),
   );
-  const filteredColumns = columnVisibility
-    .filter(({ id }) => !search || `${id}`.match(search.trim()))
-    .filter(({ visible }) => {
-      if (view === "hidden") {
-        return !visible;
-      } else if (view === "visible") {
-        return visible;
-      } else {
-        return true;
-      }
-    });
+  const filteredColumns = columnVisibility.filter(
+    ({ id }) => !search || `${id}`.match(search.trim()),
+  );
 
   useEffect(() => {
     const visibleColumns = columnVisibility
@@ -63,6 +71,30 @@ const TableSettingsColumns = ({
         : null,
     );
   }, [columnVisibility]);
+
+  const selectAllColumns = async () => {
+    for (const column of leafColumns) {
+      column.toggleVisibility(true);
+    }
+    setColumnVisibility(
+      leafColumns.map((column) => ({
+        id: column.id,
+        visible: true,
+      })),
+    );
+  };
+
+  const clearAllColumns = async () => {
+    for (const column of leafColumns) {
+      column.toggleVisibility(false);
+    }
+    setColumnVisibility(
+      leafColumns.map((column) => ({
+        id: column.id,
+        visible: false,
+      })),
+    );
+  };
 
   const ColumnRender = ({ column }) => (
     <label
@@ -89,40 +121,24 @@ const TableSettingsColumns = ({
     </label>
   );
 
-  const ViewSelector = ({
-    label,
-    view,
-    setView,
-  }: {
-    label: TableSettingsColumnsViewType;
-    view: TableSettingsColumnsViewType;
-    setView: (view: TableSettingsColumnsViewType) => void;
-  }) => (
-    <span
-      className={classNames(
-        "capitalize",
-        label === view
-          ? "font-semibold underline"
-          : "font-light cursor-pointer",
-      )}
-      onClick={label !== view ? () => setView(label) : undefined}
-    >
-      {label}
-    </span>
-  );
-
   return (
     <div className="space-y-3">
-      {/*<span className="font-semibold">Visible Columns</span>*/}
       <SearchInput
         placeholder="Search columns..."
         value={search}
         setValue={setSearch}
       />
       <div className="flex space-x-4">
-        <ViewSelector label="all" view={view} setView={setView} />
-        <ViewSelector label="visible" view={view} setView={setView} />
-        <ViewSelector label="hidden" view={view} setView={setView} />
+        <ShortcutSelector
+          icon="select_all"
+          label="Select all"
+          action={selectAllColumns}
+        />
+        <ShortcutSelector
+          icon="clear_all"
+          label="Clear"
+          action={clearAllColumns}
+        />
       </div>
       <div className="overflow-x-auto pl-px">
         {filteredColumns.map((column) => (
@@ -134,17 +150,4 @@ const TableSettingsColumns = ({
   );
 };
 
-const TableSettings = ({
-  name,
-  leafColumns,
-}: {
-  name: string;
-  leafColumns: Column<RowData>[];
-}) => {
-  return <TableSettingsColumns name={name} leafColumns={leafColumns} />;
-};
-
-export default TableSettings;
-
-// http://localhost:3000/snapshot/aws_detections.benchmark.mitre_v151.20241210T145435.pps?where=%7B%22aws_detections.benchmark.mitre_v151%22%3A%7B%22operator%22%3A%22and%22%2C%22expressions%22%3A%5B%7B%22operator%22%3A%22equal%22%2C%22value%22%3A%22aws_detections.benchmark.mitre_v151_ta0001%22%2C%22type%22%3A%22benchmark%22%7D%5D%7D%7D&grouping=%7B%22aws_detections.benchmark.mitre_v151%22%3A%5B%7B%22type%22%3A%22detection%22%7D%2C%7B%22type%22%3A%22result%22%7D%5D%7D&table=%7B%22aws_detections.detection.cloudtrail_logs_detect_iam_root_console_logins%22%3A%7B%22display_columns%22%3A%5B%22timestamp%22%2C%22operation%22%2C%22resource%22%2C%22account_id%22%2C%22region%22%2C%22tp_id%22%5D%7D%7D&input.detection_range=%7B%22from%22%3A%222024-12-09T15%3A33%3A45.126Z%22%2C%22to%22%3Anull%2C%22relative%22%3A%221d%22%7D
-//
+export default TableSettingsColumns;
