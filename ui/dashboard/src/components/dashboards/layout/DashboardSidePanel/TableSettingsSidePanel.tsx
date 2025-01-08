5b655d4e9b067e2d60e74981488aaba117f5f547
Mike Burgess <mike@turbot.com>
2025-01-08 23:00:30 +0000

    Fix scrolling of column chooser side panel

--- ui/dashboard/src/components/dashboards/layout/DashboardSidePanel/TableSettingsSidePanel.tsx
+++ ui/dashboard/src/components/dashboards/layout/DashboardSidePanel/TableSettingsSidePanel.tsx
@@ -12,19 +12,19 @@ const TableSettingsSidePanel = ({
 }) => {
   const { closeSidePanel } = useDashboardPanelDetail();
   return (
-    <div className="h-full md:min-w-[300px] md:max-w-[800px]">
-      <div className="flex items-center justify-between p-4">
-        <h3>Visible Columns</h3>
-        <Icon
-          className="w-5 h-5 text-foreground cursor-pointer hover:text-foreground-light shrink-0"
-          icon="close"
-          onClick={closeSidePanel}
-          title="Close"
-        />
+    <div className="flex flex-col h-full md:min-w-[300px] md:max-w-[800px]">
+      <div className="flex flex-col">
+        <div className="flex items-center justify-between p-4">
+          <h3>Visible Columns</h3>
+          <Icon
+            className="w-5 h-5 text-foreground cursor-pointer hover:text-foreground-light shrink-0"
+            icon="close"
+            onClick={closeSidePanel}
+            title="Close"
+          />
+        </div>
       </div>
-      <div className="p-4 pt-0">
-        <TableSettings name={panelName} leafColumns={leafColumns} />
-      </div>
+      <TableSettings name={panelName} leafColumns={leafColumns} />
     </div>
   );
 };
