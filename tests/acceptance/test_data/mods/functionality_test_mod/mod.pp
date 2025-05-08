3867b1d107ab70206579831cb32f793dac25fab6
Puskar Basu <puskar@turbot.com>
2025-05-08 19:58:58 +0530

    revert test data

--- tests/acceptance/test_data/mods/functionality_test_mod/mod.pp
+++ tests/acceptance/test_data/mods/functionality_test_mod/mod.pp
@@ -1,10 +1,4 @@
 mod "functionality_test_mod"{
   title = "Functionality test mod"
   description = "This is a simple mod used for testing different steampipe features and funtionalities."
-  database = var.database
-}
-
-variable "database" {
-  type    = connection.postgres
-  default = connection.postgres.my_connection
 }
